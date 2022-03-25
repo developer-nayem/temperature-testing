1effcb4fa119008fc91a48ebbc4ff575c8f33110
developer-nayem <mdalimodassernayem@gmail.com>
2022-03-25 07:08:11 +0600

    add condition

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const lead = document.querySelector(".lead");
 document.getElementById("button").addEventListener("click", function(){
     if(input.value == ""){
         alert("Enter The City Name")
-    }else{
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=66b77a9becc4d320c4e4e3354847983b')
+    }
+    else{
+       fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=66b77a9becc4d320c4e4e3354847983b')
         .then(res => res.json())
         .then(data => {
-        cityName.innerText = data.name;
-        temperature.innerText = `${data.main.temp}°C`;
-        lead.innerText = data.weather[0].main;
-        const img = document.createAttribute("src");
-        img.value = 'https://openweathermap.org/img/wn/'+data.weather[0].icon+'.png';
-        icon.setAttributeNode(img);
-        input.value = "";
+            if(input.value = data.name){
+                cityName.innerText = data.name;
+                temperature.innerText = `${data.main.temp}°C`;
+                lead.innerText = data.weather[0].main;
+                const img = document.createAttribute("src");
+                img.value = 'https://openweathermap.org/img/wn/'+data.weather[0].icon+'.png';
+                icon.setAttributeNode(img);
+                input.value = "";
+            }else{
+                alert("Place Enter the correct name");
+                input.value = "";
+            } 
     });  
     };
 });
